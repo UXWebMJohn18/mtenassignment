1a01c2cdb8a45dc4d443d427076257a928562c9b
UXWebMJohn18 <mejohnson18@my.waketech.edu>
2025-03-25 21:35:35 -0400

    Third commit to main

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com":uxwebmjohn18/mtenassignment.git master:gh-pages
+git push -f git@github.com":UXWebMJohn18/mtenassignment.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-module.exports = {
+module.exports = defineConfig({
   publicPath: '/mtenassignment/'
-}
+})
